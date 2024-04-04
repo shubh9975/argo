47f1a817c858bed2d7add89d4d58c8f67343b2e6
shubh9975 <68888680+shubh9975@users.noreply.github.com>
2024-04-04 13:05:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
 <html>
 <body>
 <center>
-<h1>Demo on Shubham with ArgoCD and Github Actions.</h1> <br>
+<h1>Demo on GitOps with ArgoCD and Github Actions.</h1> <br>
 <br>
 <img src="https://github.com/tanmaybhandge/CICD_Application_K8s/blob/main/itsworking.jpeg?raw=true">
 </center>
